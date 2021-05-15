e864bc32324b7a1f9a2a0b5e7cf5ed3b8b3ddda2
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2021-05-15 17:30:04 +0300

    Run the code when the content-script is loaded: ie. after the Garmin Activity page loads

--- garmin.js
+++ garmin.js
@@ -1,149 +1,151 @@
-/**
-TODO: centralise all strings, especially Garmin ones
-TODO: other UI languages?
-TODO: change activity from the arrows at the top
-TODO: Analytics: track downloads
-TODO: Full screen
-TODO: Buy me a coffee
+(function() {
+    /**
+    TODO: centralise all strings, especially Garmin ones
+    TODO: other UI languages?
+    TODO: change activity from the arrows at the top
+    TODO: Analytics: track downloads
+    TODO: Full screen
+    TODO: Buy me a coffee
 
-BUG: https://connect.garmin.com/modern/activity/6767576021
-BUG: https://connect.garmin.com/modern/activity/6767571262 ?
-*/
+    BUG: https://connect.garmin.com/modern/activity/6767576021
+    BUG: https://connect.garmin.com/modern/activity/6767571262 ?
+    */
 
-/**
- * zone - the zone for which you need the intervals on the UI
- * Return: string as seen on the UI
- */
-function getGarminZoneInterval(zone)
-{
-    return document.getElementsByClassName('heart-rate-zones')[0].getElementsByClassName('zones-' + zone)[0].getElementsByClassName('timeInZonesDetails')[0].getElementsByTagName('span')[1].innerText.trim();
-}
-
-/**
- * Get the neighbouring limits of a zone read from the UI
- * Neighbouring: always the value touches the next zone (zones 1-4) or the previous zone: where zone 5 touches zone 4
- */
-function parseGarminZoneInterval(garminZoneInterval)
-{
-    // trim strings & filter empty string since the text of the zone intervals may contain redundant white spaces
-    const tokens = garminZoneInterval.split(' ').map(x => x.trim()).filter(x => x.length > 0);
-
-    // the penultimate token is always the number before bpm, eg: "> 190 bpm" or "120 - 130 bpm"
-    // and thet is our "neighbouring" limit of the zone
-    return tokens[tokens.length - 2];
-}
-
-function getHRZoneTicks()
-{
-    var ticks = [];
-    // only 4 zones since I need to draw up to first limit, up to second, up to third, up to fourth, above the fourth
-    for(let i = 1; i <= 4; ++i)
+    /**
+     * zone - the zone for which you need the intervals on the UI
+     * Return: string as seen on the UI
+     */
+    function getGarminZoneInterval(zone)
     {
-        const gZoneInterval = getGarminZoneInterval(i);
-        const zoneLimit = parseGarminZoneInterval(gZoneInterval);
-        ticks.push(zoneLimit);
-        console.log(`Zone ${i}: ${gZoneInterval} - limit: ${zoneLimit}`);
+        return document.getElementsByClassName('heart-rate-zones')[0].getElementsByClassName('zones-' + zone)[0].getElementsByClassName('timeInZonesDetails')[0].getElementsByTagName('span')[1].innerText.trim();
     }
 
-    return ticks;
-}
-
-/**
- * minHr - on plot
- * maxHr - on plot
- */
-function hrZoneTicksToPercentages(ticks, minHr, maxHr)
-{
-    const range = maxHr - minHr;
-
-    //TODO: in (the unlikely) case the plot's minimum HR reading is above one of the HR zones, the "tick-minHr" computation will be negative, in that case set the percentage to 0
-    return ticks.map(tick => (tick-minHr)/range);
-}
-
-function createSVGElement(el, attrs)
-{
-    const svgEl = document.createElementNS("http://www.w3.org/2000/svg", el);
-    for(const [attrName, attrVal] of Object.entries(attrs))
+    /**
+     * Get the neighbouring limits of a zone read from the UI
+     * Neighbouring: always the value touches the next zone (zones 1-4) or the previous zone: where zone 5 touches zone 4
+     */
+    function parseGarminZoneInterval(garminZoneInterval)
     {
-        svgEl.setAttribute(attrName, attrVal);
+        // trim strings & filter empty string since the text of the zone intervals may contain redundant white spaces
+        const tokens = garminZoneInterval.split(' ').map(x => x.trim()).filter(x => x.length > 0);
+
+        // the penultimate token is always the number before bpm, eg: "> 190 bpm" or "120 - 130 bpm"
+        // and thet is our "neighbouring" limit of the zone
+        return tokens[tokens.length - 2];
     }
 
-    return svgEl;
-}
-
-async function main()
-{
-    document.getElementById('tabTimeInZonesId').click();
-    console.log('Clicked on "Time in Zones"')
-    document.getElementById('tabStatsId').click();
-    console.log('Clicked on "Stats"')
-
-    // wait for the Time in Zones stats to load
-    await new Promise(r => setTimeout(r, 500));
-
-    const ticks = getHRZoneTicks();
-
-    // the position of the HR plot among the other plots
-    const gHRPlotPos = [...document.getElementsByClassName('chart-name')].map(el => el.innerText).indexOf('Heart Rate');
-
-    if(gHRPlotPos !== -1)
+    function getHRZoneTicks()
     {
-        const hrPlotLabels = [...document.querySelectorAll('.highcharts-axis-labels.highcharts-yaxis-labels')[gHRPlotPos].children].map(tick => tick.getElementsByClassName('y-axis-label')[0].innerHTML);
-
-        const min = parseInt(hrPlotLabels[0]);
-        const max = parseInt(hrPlotLabels.slice(-1));
-
-        if (min > max)
+        var ticks = [];
+        // only 4 zones since I need to draw up to first limit, up to second, up to third, up to fourth, above the fourth
+        for(let i = 1; i <= 4; ++i)
         {
-            [min, max] = [max, min];
+            const gZoneInterval = getGarminZoneInterval(i);
+            const zoneLimit = parseGarminZoneInterval(gZoneInterval);
+            ticks.push(zoneLimit);
+            console.log(`Zone ${i}: ${gZoneInterval} - limit: ${zoneLimit}`);
         }
 
-        // get the HR zones as percentages and add the zone 5 as 100% for drawing purposes
-        const percentageHRLimits = [...hrZoneTicksToPercentages(ticks, min, max), 1];
-        console.log(`Percentage HR limits: ${percentageHRLimits}`);
+        return ticks;
+    }
 
-        // get the background rect of the plot
-        const gHRBgRect = document.getElementsByClassName('highcharts-plot-background')[gHRPlotPos];
+    /**
+     * minHr - on plot
+     * maxHr - on plot
+     */
+    function hrZoneTicksToPercentages(ticks, minHr, maxHr)
+    {
+        const range = maxHr - minHr;
 
-        const hrBandsDefs = createSVGElement('defs', {});
-        const hrBandsGradient = createSVGElement('linearGradient', {
-            'id': 'saucehrbandsoverlay',
-            // draw the bands horizontally
-            'x1': 0,
-            'x2': 0,
-            'y1': 1,
-            'y2': 0,
-        });
+        //TODO: in (the unlikely) case the plot's minimum HR reading is above one of the HR zones, the "tick-minHr" computation will be negative, in that case set the percentage to 0
+        return ticks.map(tick => (tick-minHr)/range);
+    }
 
-        const zonesColors = ['gray', 'blue', 'green', 'orange', 'red'];
-        for(var [limit, color] of percentageHRLimits.map((limit, index) => [limit, zonesColors[index]]))
+    function createSVGElement(el, attrs)
+    {
+        const svgEl = document.createElementNS("http://www.w3.org/2000/svg", el);
+        for(const [attrName, attrVal] of Object.entries(attrs))
         {
-            hrBandsGradient.appendChild(createSVGElement('stop', {
-                'offset': limit,
-                'stop-color': color,
-            }));
+            svgEl.setAttribute(attrName, attrVal);
         }
 
-        hrBandsDefs.appendChild(hrBandsGradient);
-
-        const hrBandsOverlay = createSVGElement('rect', {
-            'x': gHRBgRect.getAttribute('x'),
-            'y': gHRBgRect.getAttribute('y'),
-            'rx': gHRBgRect.getAttribute('rx'),
-            'ry': gHRBgRect.getAttribute('ry'),
-            'width': gHRBgRect.getAttribute('width'),
-            'height': gHRBgRect.getAttribute('height'),
-            'fill': 'url(#saucehrbandsoverlay)',
-        });
-
-        // add the HR bands after the background of the HR plot, thus rendering it over the background        
-        gHRBgRect.parentNode.insertBefore(hrBandsOverlay, gHRBgRect.nextSibling);
-        gHRBgRect.parentNode.insertBefore(hrBandsDefs, gHRBgRect.nextSibling);
+        return svgEl;
     }
-    else
+
+    async function main()
     {
-        console.log('No HR plot found');
-    }
-}
+        document.getElementById('tabTimeInZonesId').click();
+        console.log('Clicked on "Time in Zones"')
+        document.getElementById('tabStatsId').click();
+        console.log('Clicked on "Stats"')
 
-main();
+        // wait for the Time in Zones stats to load
+        await new Promise(r => setTimeout(r, 500));
+
+        const ticks = getHRZoneTicks();
+
+        // the position of the HR plot among the other plots
+        const gHRPlotPos = [...document.getElementsByClassName('chart-name')].map(el => el.innerText).indexOf('Heart Rate');
+
+        if(gHRPlotPos !== -1)
+        {
+            const hrPlotLabels = [...document.querySelectorAll('.highcharts-axis-labels.highcharts-yaxis-labels')[gHRPlotPos].children].map(tick => tick.getElementsByClassName('y-axis-label')[0].innerHTML);
+
+            const min = parseInt(hrPlotLabels[0]);
+            const max = parseInt(hrPlotLabels.slice(-1));
+
+            if (min > max)
+            {
+                [min, max] = [max, min];
+            }
+
+            // get the HR zones as percentages and add the zone 5 as 100% for drawing purposes
+            const percentageHRLimits = [...hrZoneTicksToPercentages(ticks, min, max), 1];
+            console.log(`Percentage HR limits: ${percentageHRLimits}`);
+
+            // get the background rect of the plot
+            const gHRBgRect = document.getElementsByClassName('highcharts-plot-background')[gHRPlotPos];
+
+            const hrBandsDefs = createSVGElement('defs', {});
+            const hrBandsGradient = createSVGElement('linearGradient', {
+                'id': 'saucehrbandsoverlay',
+                // draw the bands horizontally
+                'x1': 0,
+                'x2': 0,
+                'y1': 1,
+                'y2': 0,
+            });
+
+            const zonesColors = ['gray', 'blue', 'green', 'orange', 'red'];
+            for(var [limit, color] of percentageHRLimits.map((limit, index) => [limit, zonesColors[index]]))
+            {
+                hrBandsGradient.appendChild(createSVGElement('stop', {
+                    'offset': limit,
+                    'stop-color': color,
+                }));
+            }
+
+            hrBandsDefs.appendChild(hrBandsGradient);
+
+            const hrBandsOverlay = createSVGElement('rect', {
+                'x': gHRBgRect.getAttribute('x'),
+                'y': gHRBgRect.getAttribute('y'),
+                'rx': gHRBgRect.getAttribute('rx'),
+                'ry': gHRBgRect.getAttribute('ry'),
+                'width': gHRBgRect.getAttribute('width'),
+                'height': gHRBgRect.getAttribute('height'),
+                'fill': 'url(#saucehrbandsoverlay)',
+            });
+
+            // add the HR bands after the background of the HR plot, thus rendering it over the background        
+            gHRBgRect.parentNode.insertBefore(hrBandsOverlay, gHRBgRect.nextSibling);
+            gHRBgRect.parentNode.insertBefore(hrBandsDefs, gHRBgRect.nextSibling);
+        }
+        else
+        {
+            console.log('No HR plot found');
+        }
+    }
+
+    main();
+})();
